112769e0a42e79633f0035b2dba4b6c4ddd41add
Flynn <matt.etherington@coderacademy.edu.au>
2018-10-24 14:04:21 +1000

    Final version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    @post.image.attach(post_params[:image])
 
     respond_to do |format|
       if @post.save
@@ -77,6 +78,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :image)
     end
 end
